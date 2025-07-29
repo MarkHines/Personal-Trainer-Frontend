df70660c7e3730618afaad5bccc539def3157d4f
Mark Hines <hines3521@gmail.com>
2025-07-29 00:49:32 -0400

    add TrainerById route and update seed.js

--- Back-end/api/trainers.js
+++ Back-end/api/trainers.js
@@ -1,9 +1,17 @@
-import { getAllTrainers } from '#db/queries/trainers';
 import express from 'express';
 const trainersRouter = express.Router();
 export default trainersRouter;
 
+import { getAllTrainers, getTrainerById } from '#db/queries/trainers';
+
+
 trainersRouter.get(`/`, async (request, response) => {
   const trainers = await getAllTrainers();
   response.send(trainers)
+})
+
+trainersRouter.get(`/:id`, async (request, response) => {
+  const { id } = request.params;
+  const trainer = await getTrainerById(id)
+  response.send(trainer)
 })
