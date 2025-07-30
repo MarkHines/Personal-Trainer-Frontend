a71420effb1712e62563221e55452b77332d16a2
Mark Hines <hines3521@gmail.com>
2025-07-30 10:10:00 -0400

    add programs section with programs by trainer id

---
+++ Front-end/src/programs/Programs.jsx
@@ -0,0 +1,24 @@
+import useQuery from '../api/useQuery';
+import { Link } from 'react-router';
+
+
+const Programs = ({ trainer }) => {
+  const { data: programs } = useQuery(`/trainers/${trainer.id}/programs`, `programs`);
+  
+  return (
+    <>
+      <h1>Programs</h1>
+      {
+        programs && programs.map((program) => {
+          return (
+            <Link key={program.id}>
+              {program.name}
+            </Link>
+          )
+        })
+      }
+    </>
+  );
+};
+
+export default Programs
--- Front-end/src/trainers/TrainerById.jsx
+++ Front-end/src/trainers/TrainerById.jsx
@@ -1,20 +1,23 @@
 import useQuery from "../api/useQuery";
 import { useParams } from "react-router";
+import Programs from "../programs/Programs";
 
 
 const TrainerById = () => {
   const { id } = useParams();
   const { data: trainer } = useQuery(`/trainers/${id}`, `trainers`);
   if(!trainer) return null;
-  console.log(trainer)
   return (
-    <section className="trainerDetails">
-      <h1>Trainer Details</h1>
-      <h2>{trainer.name}</h2>
-      <img src={trainer.img_url} />
-      <h3>About {trainer.name}</h3>
-      <p>{trainer.bio}</p>
-    </section>
+      <>
+        <h1>Trainer Details</h1>
+        <section className="trainerDetails">
+          <h2>{trainer.name}</h2>
+          <img src={trainer.img_url} />
+          <h3>About {trainer.name}</h3>
+          <p>{trainer.bio}</p>
+        </section>
+        <Programs trainer={trainer}/>
+      </>
   );
 };
 
